14071b9ea93410240f5a1efcede3f2da4f2f0104
saisama99 <89896388+saisama99@users.noreply.github.com>
2021-09-08 06:50:32 -0700

    Add files via upload

---
+++ math functions.py
@@ -0,0 +1,70 @@
+import math
+
+'''fsum&prod(addition and multiplication)'''
+# a=[1,2,3,4,5,6,7,8,9]
+# print(math.fsum(a))               #45.0
+# print(int(math.fsum(a)))          #45
+# print(math.prod(a))               #362880
+
+'''fabs(print in positive value)'''
+# b=-55.65
+# print(math.fabs(b))              #55.65
+
+'''exp&factprial'''
+# print(math.exp(1))               #2.718281828459045
+# print(math.exp(2))               #7.38905609893065
+# print(math.exp(3))               # 20.085536923187668
+
+# print(math.factorial(5))         #120(5*4*3*3*2*1)
+# print(math.factorial(4))         #24
+# print(math.factorial(2))         #2
+# print(math.factorial(9))         #362880
+
+'''floor'''
+# print(math.floor(45.01))         #45
+# print(math.floor(45.99))         #45
+# print(math.floor(45))            #45
+
+'''ceil'''
+# print(math.ceil(45.4))           #46
+# print(math.ceil(45.6))           #46
+# print(math.ceil(45.5))           #46
+# print(math.ceil(45.00))          #46
+
+'''power'''
+# print(math.pow(3,4))             #81.0
+# print(math.pow(3,6))             #729.0
+# print(math.pow(2,4))             #16.0
+
+'''GCD'''
+# print(math.gcd(33,5))             #1
+# print(math.gcd(30,5))             #5
+# print(math.gcd(24,6))             #6
+# print(math.gcd(9,6))              #3
+
+'''LCM'''
+# print(math.lcm(33,5))             #165
+# print(math.lcm(30,5))             #30
+# print(math.lcm(24,6))             #24
+# print(math.lcm(9,6))              #18
+
+'''mod(remainder)'''
+# print(math.fmod(12,2))            #0.0
+# print(math.fmod(12,5))            #2.0
+# # print(math.fmod(13,4))          #1.0
+
+'''sqrt(square root)'''
+# print(math.sqrt(25))              #5.0
+# print(math.sqrt(45))              #6.70 
+# print(math.sqrt(125))             #11.18
+# print(math.sqrt(69))              #8.30
+
+'''isqrt(is squareroot)'''
+# print(math.isqrt(25))             #5
+# print(math.isqrt(45))             #6
+# print(math.isqrt(125))            #11
+# print(math.isqrt(69))             #8
+
+
+
+
